acb036c6b7cd81e5e833c08c00fd56fb2ae89d9a
Furkan Demir <79414596+furkanddemir@users.noreply.github.com>
2024-12-09 18:14:49 +0300

    Add files via upload

---
+++ 13_cSharpEgitimKampi301.DataAccessLayer/Abstract/IGenericDal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace cSharpEgitimKampi301.DataAccessLayer.Abstract
+{
+    public interface IGenericDal<T> where T : class
+    {
+        void Insert(T entitiy);
+        void Update(T entitiy);
+        void Delete(int id);
+        List<T> GetAll();
+        T GetById(int id);
+    }
+}
---
+++ 13_cSharpEgitimKampi301.DataAccessLayer/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace cSharpEgitimKampi301.DataAccessLayer.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<cSharpEgitimKampi301.DataAccessLayer.Context.KampContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(cSharpEgitimKampi301.DataAccessLayer.Context.KampContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
