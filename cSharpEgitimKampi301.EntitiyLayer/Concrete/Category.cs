6e9f2ea353c5707d380ff0c80995ed9c3ab4884a
Furkan Demir <79414596+furkanddemir@users.noreply.github.com>
2024-12-09 15:29:13 +0300

    Add files via upload

---
+++ cSharpEgitimKampi301.EntitiyLayer/Concrete/Category.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cSharpEgitimKampi301.EntitiyLayer.Concrete
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public bool CategoryStatus { get; set; }
+        public List<Product> Products { get; set; }
+    }
+}
+
+/*
+    Field-Variable-Property
+
+Eğer bir değişken direkt olarak sınıf içine tanımlanırsa bu bir field'dır.
+int z; --> Field
+
+Eğer o değişken yapısı sonuna  {get; set;} diye 2 değer alırsa bu bir property'dir.
+public  int y { get; set; } --> Property
+
+Eğer bir değer metot içinde tanımlanıyorsa bu bir Variable'dır.
+void test()
+{
+    int z;          --> Variable
+}
+ 
+ */
---
+++ cSharpEgitimKampi301.EntitiyLayer/Concrete/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cSharpEgitimKampi301.EntitiyLayer.Concrete
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public Product Product { get; set; }  
+        public int Quanity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal TotalPrice { get; set; }
+        public int CustomerId { get; set; }
+        public virtual Customer Customer { get; set; }
+    }
+}
