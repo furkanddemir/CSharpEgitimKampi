e8a1a31c0c3ca89fdd428395c69342d89e375fcf
Furkan Demir <79414596+furkanddemir@users.noreply.github.com>
2024-12-22 15:09:38 +0300

    Add files via upload

---
+++ 21_cSharpEgitimKampi301.PresentationLayer/FrmProduct.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using cSharpEgitimKampi301.BusinessLayer.Abstract;
+using cSharpEgitimKampi301.BusinessLayer.Concrete;
+
+
+namespace cSharpEgitimKampi301.PresentationLayer
+{
+    public partial class FrmProduct : Form
+    {
+        private readonly IProductService _productService;
+        public FrmProduct()
+        {
+            InitializeComponent();
+            _productService=new ProductManager(new EfProductDal());
+        }
+
+        
+        private void btnList_Click(object sender, EventArgs e)
+        {
+            var values = _productService.TGetAll();
+            dataGridView1.DataSource = values;
+        }
+
+        private void btnList2_Click(object sender, EventArgs e)
+        {
+            var values = _productService.TGetProductsWithCategory();
+            dataGridView1.DataSource = values;
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+
+            //productManager.TDelete()
+        }
+    }
+}
