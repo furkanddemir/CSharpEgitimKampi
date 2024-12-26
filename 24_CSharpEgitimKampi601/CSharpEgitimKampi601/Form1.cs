19fe3b45070bc5ff88c1a2650291f7421e5f340c
Furkan Demir <fd3938704@gmail.com>
2024-12-26 17:28:55 +0300

    Add files via upload

---
+++ 24_CSharpEgitimKampi601/CSharpEgitimKampi601/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CSharpEgitimKampi601.Entities;
+using CSharpEgitimKampi601.Services;
+
+namespace CSharpEgitimKampi601
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        CustomerOperations customerOperations = new CustomerOperations();
+        private void btnCustomerCreate_Click(object sender, EventArgs e)
+        {
+            var customer = new Customer()
+            {
+                CustomerName = txtCustomerName.Text,
+                CustomerSurname = txtCustomerSurname.Text,
+                CustomerBalance =txtCustomerBalance.Text,
+                CustomerCity = txtCustomerCity.Text,
+                CustomerShoppingCount =txtCustomerShoppingCount.Text,
+            };
+
+            customerOperations.AddCustomer(customer);
+            MessageBox.Show("Müşteri ekleme işlemi başarılı!", "Uyarı", MessageBoxButtons.OK,MessageBoxIcon.Information);
+        }
+    }
+}
